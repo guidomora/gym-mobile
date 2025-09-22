fc6d0dcc6823e35fd01350831a4fafbdf50b0d8b
guido morabito <guidomorabito161@hotmail.com>
2025-09-22 18:39:44 -0300

    probando estilos

--- app/src/main/java/com/example/gym_app/LoginActivity.java
+++ app/src/main/java/com/example/gym_app/LoginActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
@@ -13,7 +14,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button loginButton = findViewById(R.id.btn_login);
-        Button registerButton = findViewById(R.id.btn_register_from_login);
+        TextView registerButton = findViewById(R.id.btn_register_from_login);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
